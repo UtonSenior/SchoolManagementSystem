adfb4fbfca458f68f37c3116db0a9d56f987f565
UtonSenior <46914291+UtonSenior@users.noreply.github.com>
2019-03-27 20:01:16 -0500

    Created DB

--- SchoolManagementSystem/Controllers/HomeController.cs
+++ SchoolManagementSystem/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace SchoolManagementSystem.Controllers
 
             return View();
         }
+
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
     }
 }
